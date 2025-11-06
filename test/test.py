1024338da09e2d280912630e38b7d6555f72f60d
Remington Holder <remingtonholder@protonmail.com>
2025-11-06 15:22:36 -0600

    Set Enable back to zero to dump register

--- test/test.py
+++ test/test.py
@@ -31,6 +31,9 @@ async def test_project(dut):
     # Wait for one clock cycle to see the output values
     await ClockCycles(dut.clk, 3)
 
+    # Set the input values you want to test
+    dut.ui_in.value = 14
+
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
     assert dut.uo_out.value == 3
