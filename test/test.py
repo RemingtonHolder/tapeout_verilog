478bdd46c8603559248fe5557f8e39b743613563
Remington Holder <remingtonholder@protonmail.com>
2025-11-06 18:13:36 -0600

    make clock later

--- test/test.py
+++ test/test.py
@@ -29,13 +29,13 @@ async def test_project(dut):
     dut.ui_in.value = 15
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 2)
+    await ClockCycles(dut.clk, 100)
 
     # Set the input values you want to test
     dut.ui_in.value = 14
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 200)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
