d9edb6fa6fdb0f099102cdc19e696c3d7fdc83e6
Remington Holder <remingtonholder@protonmail.com>
2025-11-06 19:36:48 -0600

    adjust timing

--- test/test.py
+++ test/test.py
@@ -29,17 +29,17 @@ async def test_project(dut):
     dut.ui_in.value = 15
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 1)
 
     # Set the input values you want to test
     dut.ui_in.value = 14
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 1)
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 3
+    assert dut.uo_out.value == 6
 
     # Keep testing the module by changing the input values, waiting for
     # one or more clock cycles, and asserting the expected output values.
