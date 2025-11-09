2a20a61cca15902a99e50336eafef24e7494c47e
Remington Holder <remingtonholder@protonmail.com>
2025-11-08 19:39:14 -0600

    Less inverters

--- src/ring_osc.v
+++ src/ring_osc.v
@@ -52,23 +52,23 @@ module tapped_ring (
     input [2:0] tap,
     output y
 );
-    wire b0, b1, b1001, b1501, b1801, b1901, b2001, b2101, b2201, b2301;
+    wire b0, b1, b1001, b1501, b1601, b1701, b1801, b1901, b2001, b2101;
     (* keep_hierarchy *) amm_inverter        start ( .a(   b0), .y(          b1) );
     (* keep_hierarchy *) inv_chain #(.N(1000))  c0 ( .a(   b1), .y(       b1001) );
     (* keep_hierarchy *) inv_chain #(.N(500))   c1 ( .a(b1001), .y(       b1501) );
-    (* keep_hierarchy *) inv_chain #(.N(300))   c2 ( .a(b1501), .y(       b1801) );
-    (* keep_hierarchy *) inv_chain #(.N(100))   c3 ( .a(b1801), .y(       b1901) );
-    (* keep_hierarchy *) inv_chain #(.N(100))   c4 ( .a(b1901), .y(       b2001) );
-    (* keep_hierarchy *) inv_chain #(.N(100))   c5 ( .a(b2001), .y(       b2101) );
-    (* keep_hierarchy *) inv_chain #(.N(100))   c6 ( .a(b2101), .y(       b2201) );
-    (* keep_hierarchy *) inv_chain #(.N(100))   c7 ( .a(b2201), .y(       b2301) );
+    (* keep_hierarchy *) inv_chain #(.N(100))   c2 ( .a(b1501), .y(       b1601) );
+    (* keep_hierarchy *) inv_chain #(.N(100))   c3 ( .a(b1601), .y(       b1701) );
+    (* keep_hierarchy *) inv_chain #(.N(100))   c4 ( .a(b1701), .y(       b1801) );
+    (* keep_hierarchy *) inv_chain #(.N(100))   c5 ( .a(b1801), .y(       b1901) );
+    (* keep_hierarchy *) inv_chain #(.N(100))   c6 ( .a(b1901), .y(       b2001) );
+    (* keep_hierarchy *) inv_chain #(.N(100))   c7 ( .a(b2001), .y(       b2101) );
     assign y =  tap == 0 ?   b1001:
                 tap == 1 ?   b1501:
-                tap == 2 ?   b1801:
-                tap == 3 ?   b1901:
-                tap == 4 ?   b2001:
-                tap == 5 ?   b2101:
-                tap == 6 ?   b2201:
-                /*tap==7*/   b2301;
+                tap == 2 ?   b1601:
+                tap == 3 ?   b1701:
+                tap == 4 ?   b1801:
+                tap == 5 ?   b1901:
+                tap == 6 ?   b2001:
+                /*tap==7*/   b2101;
     assign b0 = y;
 endmodule
