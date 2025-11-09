712b547acfc0f5954dc269fb2a8d6bfeddf9c686
Remington Holder <remingtonholder@protonmail.com>
2025-11-09 11:55:25 -0600

    Update test.py

--- test/test.py
+++ test/test.py
@@ -22,7 +22,11 @@ async def test_project(dut):
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
+<<<<<<< Updated upstream
     await ClockCycles(dut.clk, 3)
+=======
+    await ClockCycles(dut.clk, 10)
+>>>>>>> Stashed changes
     dut.rst_n.value = 1
 
     dut._log.info("Test project behavior")
@@ -32,14 +36,18 @@ async def test_project(dut):
     dut.ui_in.value = 1
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    await ClockCycles(dut.clk, 10)
 
     # Set the input values you want to test
     # dut.ui_in.value = 14
     dut.ui_in.value = 0
 
     # Wait for one clock cycle to see the output values
+<<<<<<< Updated upstream
     await ClockCycles(dut.clk, 2)
+=======
+    await ClockCycles(dut.clk, 10)
+>>>>>>> Stashed changes
 
     # The following assersion is just an example of how to check the output values.
     # Change it to match the actual expected output of your module:
